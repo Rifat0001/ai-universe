73c2a9f19b8e05dcfeca58f911df13362c352234
Rifat0001 <mahmudrifat764@gmail.com>
2023-04-02 20:27:28 +0600

    features add

--- app.js
+++ app.js
@@ -12,14 +12,36 @@ function displayData(values) {
         console.log(value)
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="card">
-                    <img src="${value.image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                    </div>
+        <div class="col">
+        <div class="card h-100 shadow border-0 p-3">
+          <img src="${value.image}" class="card-img-top rounded-3" alt="..." width="437px" height="300px">
+          <div class="card-body">
+            <h5 class="card-title">Features</h5>
+            <p class="card-text">
+            <ol class="px-3 m-0">
+            <li class="${value.features[0] === undefined ? 'd-none' : ''}">${value.features[0]}</li>
+            <li class="${value.features[1] === undefined ? 'd-none' : ''}">${value.features[1]}</li>
+            <li class="${value.features[2] === undefined ? 'd-none' : ''}">${value.features[2]}</li>
+            <li class="${value.features[3] === undefined ? 'd-none' : ''}">${value.features[3]}</li>
+            <li class="${value.features[4] === undefined ? 'd-none' : ''}">${value.features[4]}</li>
+            </ol>
+            </p>
+          </div>
+          <div class="card-footer d-flex justify-content-between align-items-center bg-white">
+            <div>
+                <h5 class="card-title">${value.name}</h5>
+                <div class="d-flex align-items-center gap-2">
+                    <i class="fa-regular fa-calendar-days"></i>
+                    <p class="m-0 p-0">${value.published_in}</p>
                 </div>
+            </div>
+            <span>
+                <button class="btn bg-danger-subtle rounded-circle"><i class="fa-solid fa-arrow-right text-danger" onclick="fetchToolsDetail('${value.id}')" data-bs-toggle="modal"
+                data-bs-target="#exampleModal"></i></button>
+            </span>
+          </div>
+        </div>
+      </div>
         `
         container.appendChild(div);
     })

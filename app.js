48cfc88cdc059dc661587ca4737398cb48e59118
Rifat0001 <mahmudrifat764@gmail.com>
2023-04-02 22:24:10 +0600

    finis

--- app.js
+++ app.js
@@ -11,57 +11,57 @@ const toggleSpinner = isLoading => {
 // call spinner
 toggleSpinner(true);
 
-// start fetching the data from api 
-function dataLoad() {
-    const url = `https://openapi.programming-hero.com/api/ai/tools`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayData(data.data.tools))
+// Fetching tools onload
+const fetchTools = () => {
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+        .then((res) => res.json())
+        .then((data) => {
+            showTools(data.data)
+        })
 }
 
-function displayData(values) {
-    const container = document.getElementById('api-container');
-    values.slice(0, 6).forEach(value => {
-        console.log(value)
-
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <div class="col">
-        <div class="card h-100 shadow border-0 p-3">
-          <img src="${value.image}" class="card-img-top rounded-3" alt="..." width="437px" height="300px">
-          <div class="card-body">
-            <h5 class="card-title">Features</h5>
-            <p class="card-text">
-            <ol class="px-3 m-0">
-            <li class="${value.features[0] === undefined ? 'd-none' : ''}">${value.features[0]}</li>
-            <li class="${value.features[1] === undefined ? 'd-none' : ''}">${value.features[1]}</li>
-            <li class="${value.features[2] === undefined ? 'd-none' : ''}">${value.features[2]}</li>
-            <li class="${value.features[3] === undefined ? 'd-none' : ''}">${value.features[3]}</li>
-            <li class="${value.features[4] === undefined ? 'd-none' : ''}">${value.features[4]}</li>
-            </ol>
-            </p>
-          </div>
-          <div class="card-footer d-flex justify-content-between align-items-center bg-white">
-            <div>
-                <h5 class="card-title">${value.name}</h5>
-                <div class="d-flex align-items-center gap-2">
-                    <i class="fa-regular fa-calendar-days"></i>
-                    <p class="m-0 p-0">${value.published_in}</p>
-                </div>
+// Display Tools
+const showTools = (data) => {
+    const toolsContainer = document.getElementById("tools-container");
+    const sortCard = data.tools;
+    sortCard.slice(0, 6).forEach((singleTools) => {
+        toolsContainer.innerHTML += `
+          <div class="col">
+          <div class="card h-100 shadow border-0 p-3">
+            <img src="${singleTools.image}" class="card-img-top rounded-3" alt="..." width="437px" height="300px">
+            <div class="card-body">
+              <h5 class="card-title">Features</h5>
+              <p class="card-text">
+                  <ol class="px-3 m-0">
+                  <li class="${singleTools.features[0] === undefined ? 'd-none' : ''}">${singleTools.features[0]}</li>
+                  <li class="${singleTools.features[1] === undefined ? 'd-none' : ''}">${singleTools.features[1]}</li>
+                  <li class="${singleTools.features[2] === undefined ? 'd-none' : ''}">${singleTools.features[2]}</li>
+                  <li class="${singleTools.features[3] === undefined ? 'd-none' : ''}">${singleTools.features[3]}</li>
+                  <li class="${singleTools.features[4] === undefined ? 'd-none' : ''}">${singleTools.features[4]}</li>
+                  </ol>
+              </p>
             </div>
-            <span>
-            <button class="btn bg-danger-subtle rounded-circle"><i class="fa-solid fa-arrow-right text-danger" onclick="toolsDetail('${value.id}')" data-bs-toggle="modal"
-            data-bs-target="#exampleModal"></i></button>
-        </span>
+            <div class="card-footer d-flex justify-content-between align-items-center bg-white">
+              <div>
+                  <h5 class="card-title">${singleTools.name}</h5>
+                  <div class="d-flex align-items-center gap-2">
+                      <i class="fa-regular fa-calendar-days"></i>
+                      <p class="m-0 p-0">${singleTools.published_in}</p>
+                  </div>
+              </div>
+              <span>
+                  <button class="btn bg-danger-subtle rounded-circle"><i class="fa-solid fa-arrow-right text-danger" onclick="fetchToolsDetail('${singleTools.id}')" data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"></i></button>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-        `
-        container.appendChild(div);
+          `
     })
 
+
     const seeMore = document.getElementById('see-more');
-    if (values.length < 6) {
+    if (data.tools.length < 6) {
 
         seeMore.classList.add("d-none");
     }
@@ -70,18 +70,97 @@ function displayData(values) {
     }
     // stop loader
     toggleSpinner(false);
+};
 
+// loading sort tools
+const loadingSortTools = () => {
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+        .then((res) => res.json())
+        .then((data) => {
+            showSortTools(data.data)
+        })
 }
 
-function fetchMoreTools() {
-    const url = `https://openapi.programming-hero.com/api/ai/tools`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => showMoreTools(data.tools))
+// sort by date show tools
+const showSortTools = (data) => {
+    const toolsContainer = document.getElementById("tools-container");
+    toolsContainer.innerHTML = '';
+    const sortCard = data.tools;
+    customSort = (a, b) => {
+        const dataA = new Date(a.published_in);
+        // console.log(dataA)
+        const dataB = new Date(b.published_in);
+        // console.log(dataB)
+        if (dataA > dataB) {
+            return 1;
+        }
+        else if (dataA < dataB) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+    sortCard.slice(0, 6).sort(customSort).forEach((singleTools) => {
+        toolsContainer.innerHTML += `
+          <div class="col">
+          <div class="card h-100 shadow border-0 p-3">
+            <img src="${singleTools.image}" class="card-img-top rounded-3" alt="..." width="437px" height="300px">
+            <div class="card-body">
+              <h5 class="card-title">Features</h5>
+              <p class="card-text">
+                  <ol class="px-3 m-0">
+                  <li class="${singleTools.features[0] === undefined ? 'd-none' : ''}">${singleTools.features[0]}</li>
+                  <li class="${singleTools.features[1] === undefined ? 'd-none' : ''}">${singleTools.features[1]}</li>
+                  <li class="${singleTools.features[2] === undefined ? 'd-none' : ''}">${singleTools.features[2]}</li>
+                  <li class="${singleTools.features[3] === undefined ? 'd-none' : ''}">${singleTools.features[3]}</li>
+                  <li class="${singleTools.features[4] === undefined ? 'd-none' : ''}">${singleTools.features[4]}</li>
+                  </ol>
+              </p>
+            </div>
+            <div class="card-footer d-flex justify-content-between align-items-center bg-white">
+              <div>
+                  <h5 class="card-title">${singleTools.name}</h5>
+                  <div class="d-flex align-items-center gap-2">
+                      <i class="fa-regular fa-calendar-days"></i>
+                      <p class="m-0 p-0">${singleTools.published_in}</p>
+                  </div>
+              </div>
+              <span>
+                  <button class="btn bg-danger-subtle rounded-circle"><i class="fa-solid fa-arrow-right text-danger" onclick="fetchToolsDetail('${singleTools.id}')" data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"></i></button>
+              </span>
+            </div>
+          </div>
+        </div>
+          `
+    })
+    const seeMore = document.getElementById('see-more');
+    if (data.tools.length < 6) {
+
+        seeMore.classList.add("d-none");
+    }
+    else {
+        seeMore.classList.remove("d-none");
+    }
+    // stop loader
+    toggleSpinner(false);
+};
+
+// loading more tools
+const loadMoreTools = () => {
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+        .then((res) => res.json())
+        .then((data) => {
+            // fetchData = data.data;
+            showMoreTools(data.data)
+        })
 }
 
+// displaying more tools
 const showMoreTools = (data) => {
-    const toolsContainer = document.getElementById("api-container");
+    // capture categories container to append all the category links
+    const toolsContainer = document.getElementById("tools-container");
     const seeMore = document.getElementById('see-more');
     if (data.tools.length < 6) {
 
@@ -144,9 +223,8 @@ const showMoreTools = (data) => {
 
 };
 
-
 // fetching tools detail
-const toolsDetail = (id) => {
+const fetchToolsDetail = (id) => {
     let url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
     fetch(url)
         .then((res) => res.json())
@@ -214,4 +292,5 @@ const showToolsDetail = (toolsDetail) => {
   </div>
       `;
 };
-dataLoad();
+
+fetchTools();
